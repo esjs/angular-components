dbc51db621cdb62b58440f39987298c34925b4b6
esjs <vladimiru.panov@gmail.com>
2017-01-29 14:12:58 +0200

    Added basic Angular cli project with Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { MaterialModule } from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
